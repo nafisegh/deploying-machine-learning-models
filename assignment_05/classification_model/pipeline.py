4598c409d3b497cffa11b9c2423b4ff2922d9b1e
nafisegh <53907169+nafisegh@users.noreply.github.com>
2025-07-13 16:03:39 -0400

    Update pipeline.py

--- assignment_05/classification_model/pipeline.py
+++ assignment_05/classification_model/pipeline.py
@@ -1,1 +1,46 @@
+from feature_engine.imputation import (
+    CategoricalImputer,
+    AddMissingIndicator,
+    MeanMedianImputer)
 
+# for encoding categorical variables
+from feature_engine.encoding import (
+    RareLabelEncoder,
+    OneHotEncoder
+)
+# set up the pipeline
+titanic_pipe = Pipeline([
+
+    # ===== IMPUTATION =====
+    # impute categorical variables with string missing
+    ('categorical_imputation', CategoricalImputer(
+        imputation_method='missing', variables=CATEGORICAL_VARIABLES)),
+
+    # add missing indicator to numerical variables
+    ('missing_indicator', AddMissingIndicator(variables=NUMERICAL_VARIABLES)),
+
+    # impute numerical variables with the median
+    ('median_imputation', MeanMedianImputer(
+        imputation_method='median', variables=NUMERICAL_VARIABLES)),
+
+
+    # Extract letter from cabin
+    ('extract_letter', ExtractLetterTransformer(variables=CABIN)),
+
+
+    # == CATEGORICAL ENCODING ======
+    # remove categories present in less than 5% of the observations (0.05)
+    # group them in one category called 'Rare'
+    ('rare_label_encoder', RareLabelEncoder(
+        tol=0.05, n_categories=1, variables=CATEGORICAL_VARIABLES)),
+
+
+    # encode categorical variables using one hot encoding into k-1 variables
+    ('categorical_encoder', OneHotEncoder(
+        drop_last=True, variables=CATEGORICAL_VARIABLES)),
+
+    # scale
+    ('scaler', StandardScaler()),
+
+    ('Logit', LogisticRegression(C=0.0005, random_state=0)),
+])
